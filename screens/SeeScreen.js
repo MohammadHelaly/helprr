3d2c9fa40a221e784ce061a798194be6e7729b9b
MohammadHelaly <mohammad.helaly@gmail.com>
2024-01-28 22:09:09 +0200

    Add trial object detection to camera

--- screens/SeeScreen.js
+++ screens/SeeScreen.js
@@ -5,8 +5,15 @@ import {
 	useCameraPermission,
 	useMicrophonePermission,
 	useCameraDevice,
+	useFrameProcessor,
 	Camera,
 } from "react-native-vision-camera";
+import {
+	Tensor,
+	TensorflowModel,
+	useTensorflowModel,
+} from "react-native-fast-tflite";
+import { useResizePlugin } from "vision-camera-resize-plugin";
 import Label from "../components/SeeScreen/Label";
 import theme from "../constants/theme";
 
@@ -17,6 +24,42 @@ const SeeScreen = (props) => {
 	const device = useCameraDevice("back");
 	const isFocused = useIsFocused();
 	const [label, setLabel] = useState(undefined);
+
+	const model = useTensorflowModel(
+		require("../assets/models/efficientdet.tflite")
+	);
+	const actualModel = model.state === "loaded" ? model.model : undefined;
+
+	useEffect(() => {
+		if (actualModel == null) return;
+		console.log("Model loaded!");
+	}, [actualModel]);
+
+	const { resize } = useResizePlugin();
+
+	const frameProcessor = useFrameProcessor(
+		(frame) => {
+			"worklet";
+
+			if (actualModel == null) return;
+
+			console.log(`Running inference on ${frame}`);
+			const resizedFrame = resize(frame, {
+				size: {
+					width: 320,
+					height: 320,
+				},
+				pixelFormat: "rgb",
+				dataType: "uint8",
+			});
+			// console.log(`Running inference on ${resizedFrame}`);
+			const result = actualModel.runSync([resizedFrame]);
+			console.log("Result: " + result);
+			const num_detections = result[3]?.[0] ?? 0;
+			console.log("Result: " + num_detections);
+		},
+		[actualModel]
+	);
 
 	useEffect(() => {
 		// if (!hasMicrophonePermission) {
@@ -33,6 +76,7 @@ const SeeScreen = (props) => {
 				style={styles.camera}
 				device={device}
 				isActive={isFocused}
+				frameProcessor={frameProcessor}
 			/>
 			{/* <View style={styles.camera} /> */}
 			<Label label={label} isFocused={isFocused} />
